faab6b07d4eaed5f6e99e580b7ca755a8836b813
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2019-09-25 12:56:09 -0300

    ASPTreino

--- TreinoAspNetCore2_2/Program.cs
+++ TreinoAspNetCore2_2/Program.cs
@@ -19,6 +19,7 @@ namespace TreinoAspNetCore2_2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseIISIntegration(); // Define o IIS como Proxy
     }
 }
