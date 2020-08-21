589c603e624669e30883575e855d97d081e681e4
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2020-08-21 17:32:03 -0300

    ASPTreino

--- TreinoAspNetCore2_2/Models/ClienteModel.cs
+++ TreinoAspNetCore2_2/Models/ClienteModel.cs
@@ -39,7 +39,8 @@ namespace TreinoAspNetCore2_2.Models
                 DAL objDAL = new DAL();
                 objDAL.LimparParametros();
                 // Usando Texto como permite a Classe DAL e uma View (VClientes) Criada no BD.
-                DataTable dt = objDAL.ExecutaConsulta(CommandType.Text, "Select Id, Nome, CPF, DataNascimento, LimiteDeCredito From Clientes order by NOME");
+                String sql = "Select Id, Nome, CPF, DataNascimento, LimiteDeCredito From Clientes order by NOME";
+                DataTable dt = objDAL.RetDatatable(sql);
 
                 for (int i = 0; i < dt.Rows.Count; i++)
                 {
@@ -72,7 +73,8 @@ namespace TreinoAspNetCore2_2.Models
             ClienteModel item;
             DAL objDAL = new DAL();
             objDAL.LimparParametros();
-            DataTable dt = objDAL.ExecutaConsulta(CommandType.Text, $"Select Id, Nome, CPF, DataNascimento, LimiteDeCredito From Clientes Where CPF = '{cpf}' order by NOME");
+            String sql = $"Select Id, Nome, CPF, DataNascimento, LimiteDeCredito From Clientes Where CPF = '{cpf}' order by NOME";
+            DataTable dt = objDAL.RetDatatable(sql);
            
             for (int i = 0; i < dt.Rows.Count; i++)
             {
